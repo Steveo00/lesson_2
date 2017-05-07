f48b7f4c2b948b832188b2b1c83692bbd3406793
Steveo00 <srwillis@ozemail.com.au>
2017-05-08 02:03:53 +1000

    improved calculator file

--- calculator.rb
+++ calculator.rb
@@ -1,24 +1,109 @@
-# ask the user for two numbers
-# ask the user for the operation to perform
-# perform the operation on the two numbers
-# display the result
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-puts "Welcome to the Calculator"
-
-puts "Please enter your first number"
-number1 = gets.chomp
-puts "Please enter your second_number"
-number2 = gets.chomp
-puts "What operation would you like to perform."
-puts "Please enter 1) to add, 2) to subtract, 3) to multiply and 4) to divide"
-operation = gets.chomp
-
-if operation == '1'
-  then puts "The answer is #{number1.to_i + number2.to_i}"
-elsif operation == '2'
-  then puts "The answer is #{number1.to_i - number2.to_i}"
-elsif operation == '3'
-  then puts "The answer is #{number1.to_i * number2.to_i}"
-else
-       puts "The answer is #{number1.to_f / number2.to_f}"
+def prompt(message)
+  puts "=> #{message}"
 end
+
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def number?(num)
+  num.to_i.to_s == num || num.to_f.to_s == num
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+prompt(MESSAGES['welcome'])
+
+name = ''
+
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}")
+
+loop do
+  number1 = ''
+  number2 = ''
+  loop do
+    prompt(MESSAGES['first_number'])
+    number1 = gets.chomp
+
+    if number?(number1)
+      break
+    else
+      puts(MESSAGES['valid_number_check'])
+    end
+  end
+
+  loop do
+    prompt(MESSAGES['second_number'])
+    number2 = gets.chomp
+
+    if number?(number2)
+      break
+    else
+      puts(MESSAGES['valid_number_check'])
+    end
+  end
+
+  operation_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operation_prompt)
+
+  operation = ''
+
+  loop do
+    operation = gets.chomp
+
+    if %w(1 2 3 4).include?(operation)
+      break
+    else
+      prompt(MESSAGES['operation_check'])
+    end
+  end
+
+  prompt("#{operation_to_message(operation)} the two numbers....")
+
+  result =  case operation
+            when '1'
+              number1.to_f + number2.to_f
+            when '2'
+              number1.to_f - number2.to_f
+            when '3'
+              number1.to_f * number2.to_f
+            when '4'
+              number1.to_f / number2.to_f
+            end
+  prompt("The answer is #{result}")
+  prompt(MESSAGES['another_operation_check'])
+  answer = gets.chomp
+  break unless answer.downcase == 'y'
+end
+
+prompt(MESSAGES['farewell'])
