b310ac8203f970952a36e5505fcdd02fe87644e0
Steveo00 <srwillis@ozemail.com.au>
2017-05-15 23:45:14 +1000

    bonus features for rps

--- rpssl.rb
+++ rpssl.rb
@@ -1,4 +1,4 @@
-VALID_CHOICES = %w(rock paper scissors spock lizard)
+VALID_CHOICES = %w(rock paper scissors spock lizard r p s l)
 
 WINNING_COMBINATIONS = { 'rock' => ['lizard', 'scissors'],
                          'paper' => ['rock', 'spock'],
@@ -26,26 +26,61 @@ def display_results(player, computer)
 end
 
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      puts "That is not a valid choice."
+  player_score = 0
+  computer_score = 0
+
+  while player_score < 5 && computer_score < 5
+    choice = ''
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
+      if VALID_CHOICES.include?(choice)
+        choice = 'rock' if choice == 'r'
+        choice = 'paper' if choice == 'p'
+        choice = 'lizard' if choice == 'l'
+        if choice == 's'
+          prompt("I'm not sure whether you mean scissors or spock.")
+          prompt('Please type in full.')
+          choice = gets.chomp
+          loop do
+            if choice != ('spock' || 'scissors')
+              prompt('Did you mean spock or scissors?')
+              choice = gets.chomp
+            end
+            break
+          end
+        end
+        break
+      else
+        puts "That is not a valid choice."
+      end
     end
+
+    computer_choice = %w(rock paper scissors spock lizard).sample
+
+    puts "You chose: #{choice}. The computer chose: #{computer_choice}"
+
+    display_results(choice, computer_choice)
+
+    if win?(choice, computer_choice)
+      player_score += 1
+    end
+    if win?(computer_choice, choice)
+      computer_score += 1
+    end
+    puts "The player score is #{player_score}."
+    puts "The computer score is #{computer_score}."
   end
 
-  computer_choice = VALID_CHOICES.sample
+  if player_score == 5
+    puts "Player is the Grand Winner."
+  else
+    puts "Computer is the Grand Winner."
+  end
 
-  puts "You chose: #{choice}. The computer chose: #{computer_choice}"
-
-  display_results(choice, computer_choice)
-
-  puts "Do you want to play again?"
-  answer = gets.chomp.downcase
-  break unless answer.start_with?('y')
+  puts "Would you like to play again? (y/n)"
+  play_again = gets.chomp.downcase
+  break if play_again != 'y'
 end
 
 puts "Thank you for playing. Goodbye!"
